3f35be0905845237e603094fcbbe3f30e0b0dfa9
Idelilah <ruthtirusew944@gmail.com>
2023-01-03 21:03:35 +0300

    bruteforce debug

--- two-sum.py
+++ two-sum.py
@@ -1,6 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        number = []
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i+1,len(nums)):
+                if target == 0:
+                    if nums[i] == 0 and nums[j] == 0:
+                        return i, j
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return i, j
